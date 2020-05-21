22600ddfc1c1c8650afbb66e2adbb9e36a1597c7
CamiloPenagos99 <penagoscamilo99@gmail.com>
2020-05-21 04:31:58 +0000

    Proxy balancer ngnix

---
+++ client2/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-system-bar dark color="primary">
+      <v-spacer></v-spacer>
+    </v-system-bar>
+    <v-content>
+      <NamesTable />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NamesTable from "./components/NamesTable";
+
+export default {
+  name: "App",
+
+  components: {
+    NamesTable
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
